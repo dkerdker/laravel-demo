2d5336065e767e24b99649f93a5ef5bce2b451b4
Dee Ker <dker95@icloud.com>
2020-08-10 22:35:35 +0800

    add search function

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -11,25 +11,26 @@ class SearchController extends Controller
     {
         $searchTerm = $request->searchTerm;
         $searchIn = $request->searchIn;
-
-        if ($searchIn != "") {
-            $results = Personnel::where(function($query) use ($searchTerm){
-                $query->where("%$searchIn%", 'LIKE', "%$searchTerm%");
-            })->latest()->paginate(5);
-        } else {
-            $results = Personnel::where(function($query) use ($searchTerm){
-                $query->where('name', 'LIKE', "%$searchTerm%")->orWhere('email', 'LIKE', "%$searchTerm%")->orWhere('details', 'LIKE', "%$searchTerm%");
-            })->latest()->paginate(5);
-        }
+        error_log($searchIn);
 
         // if ($searchIn != "") {
-        //     $results = Personnel::where("%$searchIn%", 'LIKE', "%$searchTerm%")->latest()->paginate(5);
+        //     $results = Personnel::where(function($query) use ($searchTerm, $searchIn){
+        //         $query->where("$searchIn", 'LIKE', "%$searchTerm%");
+        //     })->latest()->paginate(5);
         // } else {
-        //     $results = Personnel::where('name', 'LIKE', "%$searchTerm%")
-        //     ->orWhere('email', 'LIKE', "%$searchTerm%")
-        //     ->orWhere('details', 'LIKE', "%$searchTerm%")
-        //     ->paginate(5);
+        //     $results = Personnel::where(function($query) use ($searchTerm){
+        //         $query->where('name', 'LIKE', "%$searchTerm%")->orWhere('email', 'LIKE', "%$searchTerm%")->orWhere('details', 'LIKE', "%$searchTerm%");
+        //     })->latest()->paginate(5);
         // }
+
+        if ($searchIn != "") {
+            $results = Personnel::where("$searchIn", 'LIKE', "%$searchTerm%")->latest()->paginate(5);
+        } else {
+            $results = Personnel::where('name', 'LIKE', "%$searchTerm%")
+            ->orWhere('email', 'LIKE', "%$searchTerm%")
+            ->orWhere('details', 'LIKE', "%$searchTerm%")
+            ->paginate(5);
+        }
 
         return $results;
     }
